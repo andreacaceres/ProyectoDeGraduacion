345bae9f6b9786ac63e4ddfbe68e03c1c3746c65
Andrea Caceres <andreacaceresm@gmail.com>
2014-03-31 23:56:45 -0500

    Guardando parametros para la localizacion

--- src/ec/cacehure/classfinder/pantalla_haciaDondeIr.java
+++ src/ec/cacehure/classfinder/pantalla_haciaDondeIr.java
@@ -1,10 +1,17 @@
 package ec.cacehure.classfinder;
 
+import java.util.List;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,12 +22,22 @@ public class pantalla_haciaDondeIr extends Activity{
 
 	Button btn_salir, btn_actualizar;
 	TextView textConnected, textSsid, textBssid, textRssi;
+	//LOCALIZACION
+	WifiManager allwifi;
+	WifiScanReceiver wifiReciever;
+	String BSSIDValido[];
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.togo);
+		
+		//Localizacion
+		allwifi = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+		wifiReciever = new WifiScanReceiver();
+		allwifi.startScan();
+		//END
 		
 		textConnected = (TextView)findViewById(R.id.textconnected);       
         textSsid = (TextView)findViewById(R.id.textssidvalue);
@@ -35,6 +52,7 @@ public class pantalla_haciaDondeIr extends Activity{
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				Log.v("============= Android", "EXITING APLICACTION ");
 				finish();
 			}
 		});
@@ -46,11 +64,29 @@ public class pantalla_haciaDondeIr extends Activity{
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				//Actualizar los datos
+				Log.v("============= Android", "UPDATING DATA ");
 				DisplayWifiState();
+				allwifi.startScan();
 			}
 		});
+		
+		
 	}
 	
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		unregisterReceiver(wifiReciever);
+		super.onPause();
+	}
+		@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+			registerReceiver(wifiReciever, new IntentFilter(
+					WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+		super.onResume();
+	}
+
 	//Obtiene los valores del AP
     private void DisplayWifiState(){
     	WifiManager myWifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
@@ -59,5 +95,31 @@ public class pantalla_haciaDondeIr extends Activity{
 		textSsid.setText(myWifiInfo.getSSID());
 		textBssid.setText(myWifiInfo.getBSSID());			
 		textRssi.setText(String.valueOf(myWifiInfo.getRssi()));
+    }
+    
+    //Clase WifiScanReceiver
+    class WifiScanReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context c, Intent intent) {
+			// TODO Auto-generated method stub
+			//Estan todas las wifi detectadas
+			List<ScanResult> wifiScanList = allwifi.getScanResults();
+			final int n = wifiScanList.size(); //El tamaño de la lista
+			BSSIDValido = new String[n];
+			for(int i= 0; i<n; i++){
+				if((wifiScanList.get(i).BSSID).equalsIgnoreCase("C0:F8:DA:AC:BE:0A")){
+	        		 BSSIDValido[i] = ((wifiScanList.get(i)).toString());
+	        		 Log.v("============= Android", "EL BBSID VALIDO ES: "+wifiScanList.get(i).BSSID);
+	        		 Log.v("============= Android", "EL SSID  VALIDO ES: "+wifiScanList.get(i).SSID);
+	        		 Log.v("============= Android", "EL RSSI  VALIDO ES: "+wifiScanList.get(i).level);
+	        	}else{
+	        		 Log.v("============= Android", "EL BBSID NO VALIDO ES: "+wifiScanList.get(i).BSSID);
+	        		 Log.v("============= Android", "EL SSID  NO VALIDO ES: "+wifiScanList.get(i).SSID);
+	        		 Log.v("============= Android", "EL RSSI  NO VALIDO ES: "+wifiScanList.get(i).level);
+	        	}	
+			}
+		}
+    	
     }
 }
